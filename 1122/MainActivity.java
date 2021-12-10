3c6c74d73464f03e10cffaece77790b680ad3467
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-12-10 13:21:53 +0900

    Add files via upload

---
+++ 1122/MainActivity.java
@@ -0,0 +1,38 @@
+package com.cookandroid.study_1122;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("메인 액티비티 (수정)");
+
+        final RadioButton rdoSecond = (RadioButton) findViewById(R.id.rdoSecond);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent;
+
+                if (rdoSecond.isChecked() == true)
+                    intent = new Intent(getApplicationContext(),
+                            SecondActivity.class);
+                else
+                    intent = new Intent(getApplicationContext(),
+                            ThirdActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+}
